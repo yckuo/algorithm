89ebddbe6f50848e782ef0f2ca7797822b6ab6fb
U-DESKTOP-L3TJHER\yckuo <kokokoko@gmail.com>
2017-02-04 15:22:30 -0800

    kmp correction and review

--- kmp/kmp.cpp
+++ kmp/kmp.cpp
@@ -5,17 +5,26 @@ using namespace std;
 
 class Solution {
 public:
-    int kmp(string str1, string str2) {
-        int N1 = str1.size(), N2 = str2.size();
-        vector<int> p(N1, 0);
-        for (int i=1, j=0; i<N1; i++) {
-            while (j > 0 && str1[j] != str1[i]) j = p[j-1];
-            if (str1[j] == str1[i]) p[i] = ++j;
+    vector<int> prefix(string needle) {
+        int N = needle.size();
+        vector<int> ret(N, 0);
+        for (int j=0, i=1; i<N; i++) {
+            while (j > 0 && needle[i] != needle[j]) j = ret[j-1];
+            if (needle[i] == needle[j]) j++;
+            ret[i] = j;
         }
+        return ret;
+    }
 
-        for (int i=0, j=0; i<N2; i++) {
-            while (j > 0 && str1[j] != str2[i]) j = p[j-1];
-            if (str1[j] == str2[i]) if (++j == N1) return i - N1 + 1;
+    int kmp(string needle, string haystack) {
+        int N1 = needle.size(), N2 = haystack.size();
+        vector<int> p = prefix(needle);
+
+        for (int j=0, i=0; i<N2; i++) {
+            while (j > 0 && needle[j] != haystack[i]) j = p[j-1];
+            if (needle[j] == haystack[i]) j++;
+            cout << j << " ";
+            if (j == N1) return i - N1 + 1;
         }
 
         return -1;
@@ -26,9 +35,17 @@ int main(int argc, char** argv) {
     freopen(argv[1], "r", stdin);
 
     Solution sol;
-    string str1, str2;
-    while (cin >> str1 >> str2) {
-        cout << sol.kmp(str1, str2) << ";" << str1 << ";" << str2 << endl;
+    string needle, haystack;
+    while (cin >> needle >> haystack) {
+        cout << needle << endl;
+        cout << haystack << endl;
+        cout << "prefix = ";
+        vector<int> prefix = sol.prefix(needle);
+        for (int i=0; i<prefix.size(); i++) cout << prefix[i] << " ";
+        cout << endl;
+
+        int idx = sol.kmp(needle, haystack);
+        cout << endl << idx << endl << endl;
     }
 
     return 0;
